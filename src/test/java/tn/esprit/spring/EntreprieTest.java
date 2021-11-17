29acf491db6da6a3505e2f355d6c068f7f5bc090
souhaib-hsoumi <souhaib.hsoumi1@esprit.tn>
2021-11-17 12:10:43 +0100

    scm

--- src/test/java/tn/esprit/spring/EntreprieTest.java
+++ src/test/java/tn/esprit/spring/EntreprieTest.java
@@ -2,16 +2,11 @@ package tn.esprit.spring;
 
 
 import java.util.List;
-
-
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Entreprise;
 import tn.esprit.spring.repository.DepartementRepository;
