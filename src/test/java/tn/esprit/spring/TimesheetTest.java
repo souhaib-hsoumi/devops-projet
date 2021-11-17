b49eee693f4f7ad0468ed302edd1eaefaf68ab34
rihab83 <60881923+rihab83@users.noreply.github.com>
2021-11-17 11:52:56 +0100

    testing

--- src/test/java/tn/esprit/spring/TimesheetTest.java
+++ src/test/java/tn/esprit/spring/TimesheetTest.java
@@ -103,7 +103,6 @@ public class TimesheetTest {
 		timesheetPK.setDateFin(date);
 		timesheetPK.setIdEmploye(1);
 		timesheetPK.setIdMission(7);
-		
 		Timesheet timesheet = new Timesheet();
 		timesheet.setTimesheetPK(timesheetPK);
 		timesheet.setValide(false); 
